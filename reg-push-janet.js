4e9a3be7d0452724937070b4220357701dacd52e
Tony Finch <fanf2@cam.ac.uk>
2015-05-08 16:05:11 +0100

    DNS/reg-push-janet: WIP

--- reg-push-janet.js
+++ reg-push-janet.js
@@ -1,5 +1,15 @@
 /*
- * Drive the naming.ja.net/dns web site
+ * Drive the https://naming.ja.net/dns/ web site
+ *
+ * This site relies a lot on Javascript, which is why we are using
+ * PhantomJS and CasperJS rather than trying to treat the web server
+ * like a REST-ish API endpoint.
+ *
+ * We are mostly relying on element IDs for navigating the site. But the whole
+ * thing smells of being generated with a complicated framework, so there is
+ * little chance that these IDs will remain stable if the site is revised.
+ *
+ * Sadly I suspect this script will be rather fragile.
  */
 
 "use strict";
@@ -74,13 +84,15 @@ casper.then(function choose_domain() {
     this.click('#MainContent_btnFilter');
 });
 
-casper.then(function open_domain() {
-    info("Loaded domain list: " + this.getTitle());
+casper.then(function find_domain() {
+    info("Loaded filtered list: " + this.getTitle());
     for (var i = 0; ; i++) {
 	var sel = '#MainContent_DomainListView_ViewDomainNumber'+i+'_'+i;
 	if (!this.exists(sel))
 	    fail('Could not find domain: '+domain);
-	// I suspect this might be a bit shaky...
+	// Unfortunately there isn't a sensible selector for each row of the
+	// search results table, so we have to dig around for the cousin node
+	// which might contain the name of the domain we are looking for.
 	var res = this.evaluate(function find_domain(sel) {
 	    var a = document.querySelector(sel);
 	    var tr = a.parentNode.parentNode;
@@ -92,6 +104,14 @@ casper.then(function open_domain() {
 	    this.click(sel);
 	    break;
 	}
+    }
+});
+
+casper.then(function open_domain() {
+    info("Loaded domain details: " + this.getTitle());
+    var tbl = this.getElemensInfo('#MainContent_nameServersTab > td');
+    for (var i = 0; i < tbl.length; i++) {
+	info(tbl[i].text);
     }
 });
 
