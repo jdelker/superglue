f9f155aac1239c05e2017c4666381395467e79fb
Tony Finch <dot@dotat.at>
2015-05-19 22:54:01 +0100

    Superglue.pm: check for spurious glue when there are no NS records

--- lib/superglue/perl5/Superglue.pm
+++ lib/superglue/perl5/Superglue.pm
@@ -147,17 +147,17 @@ sub read_delegation {
 	# input (dnssec-dsfromkey will do syntax checks)
 	sdie "$z: no delegation records in input"
 	    unless $d{NS} or $d{DS} or $d{DNSKEY};
+	my %ns;
 	if ($d{NS}) {
-		my %ns;
 		for my $ns (@{$d{NS}}) {
 			sdie "$z: glue records missing for NS $ns"
 		    if $ns =~ $subdomain and not $d{glue}{$ns};
 			$ns{$ns} = 1;
 		}
-		for my $ns (keys %{$d{glue}}) {
-			sdie "$z: glue records for nonexistent NS $ns"
-			    unless $ns{$ns};
-		}
+	}
+	for my $ns (keys %{$d{glue}}) {
+		sdie "$z: glue records for nonexistent NS $ns"
+		    unless $ns{$ns};
 	}
 	return %d;
 }
